4ae4b6c0071bfdb2eab63bcc7abae7179b988f9f
Azimboy <azimboy444@gmail.com>
2022-04-09 21:26:28 +0300

    1

--- data/config.py
+++ data/config.py
@@ -6,5 +6,5 @@ env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = "5274713576:AAFP8NvZwZ8dUamqtMgTkgYGGqo7ZNskByo"  # Bot token
-ADMINS = "1076893638" # adminlar ro'yxati
+ADMINS = [1076893638] # adminlar ro'yxati
 PAYME_TOKEN = "371317599:TEST:1649355382504"  # Payments CLICK token
